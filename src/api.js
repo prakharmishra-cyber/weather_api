63ae15f601ebe4e3a74d73794ce6da602bb11cd6
Prakhar Mishra <60056480+prakharmishra-cyber@users.noreply.github.com>
2022-08-06 19:58:20 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,4 +8,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "88d84842a1c23381be3a7f582a2b2d77"; // enter your key from openweather API
